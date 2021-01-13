34aca7919a80a2fa3c001a070788a56649dfd474
gidspen <70455381+gidspen@users.noreply.github.com>
2021-01-13 15:26:26 -0600

    wip

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
 import Words from './components/words'
+import React, { useState } from 'react';
+
+
+function MyComponent() {
+  return <div>
+    My String
+  </div>
+}
+
+function MyOtherComponent() {
+  return <div>
+    My Second String
+  </div>
+}
+
 
 function App() {
+  const [ShowComponent, setShowComponent] = useState(false);
   return (
     <div className="App">
       <header className="App-header">
-        My Meditation Notes
+        My Meditation Notes {ShowComponent.toString()}
       </header>
+      {ShowComponent ? <MyComponent /> : <MyOtherComponent />}
       <Words />
+      <button onClick={ () => setShowComponent(!ShowComponent)}>
+        Button
+      </button>
     </div>
   );
 }
--- src/components/words.js
+++ src/components/words.js
@@ -1,45 +1,87 @@
 import React, { useState } from 'react';
 import Lines from './lines';
-
-const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-const recognition = new SpeechRecognition();
-
-recognition.continous = true;
-recognition.interimResults = true;
-recognition.lang = 'en-US';
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 
 
 
 export default function Words() {
+    const supportedBrowsers = ['Chrome', 'Edge', 'Chrome Android', 'WebView Android', 'Samsumg Internet'];
+    const [isActive, setIsActive] = useState(true);
+    const [transcriptList, setTranscriptList] = useState([]);
+    const { transcript, resetTranscript, finalTranscript, listening } = useSpeechRecognition();
 
-    // const [text, setText] = useState([]);
-    const text = [];
+    console.log('from the top', listening, isActive);
 
-    console.log('inside useEff', recognition);
+    if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
+        return (
+            <div className={'sorry'}>
+                <h5>
+                Sorry! This browser does not currectly support speech recognition
+                </h5>
+                <p>
+                Try one of the following:
+                    <ul>
+                        {supportedBrowsers.map(browser => <li>{browser}</li>)}
+                    </ul>
+                </p>
+            </div>
+        )
+    }
+
+    console.log('is listening: ', listening);
+
+    async function start() {
+        console.log('started startListening');
+        await SpeechRecognition.startListening({ continuous: false })
+        console.log('finished startListening');
+    }
+
+    async function stop() {
+        console.log('stopping');
+        console.log('final t: ', finalTranscript);
+        await SpeechRecognition.stopListening()
+        console.log('final y after: ', finalTranscript);
+        await setIsActive(false);
+
+        console.log('completed stop');
+    }
+
+    console.log('not listening and is active: ', (!listening && isActive));
+
+    // appends the transcript once the we stop listening
+    // then starts listening again
+    if (!listening && isActive) {
+        console.log('inside the big if!');
         
-    recognition.addEventListener('result', e => {
-        console.log(e.results);
-        const transcript = Array.from(e.results)
-        .map(result => result[0].transcript)
-        .join('');
-        
-        console.log(transcript);
-        // setText([...text, transcript])
-        text.push(transcript);
+        if (transcriptList[transcriptList.length - 1 ] !== finalTranscript ) {
+            console.log('setting transcript');
+            setTranscriptList([...transcriptList, transcript]);
+            console.log('transcript set');
         }
-    )
-    
-    console.log('this is the text', text);
+        console.log('the current tList: ', transcriptList);
 
-    recognition.addEventListener('end', recognition.start);
-    
-    recognition.start();
+        start();
+    };
 
-
-    
     return (
-        <div>Words
-           <Lines text={text}/>
+        <div>
+          <button onClick={() => setIsActive(true)}>Start</button>
+          <button onClick={stop}>Stop</button>
+          <button onClick={resetTranscript}>Reset</button>
+            <ul>
+              {/* {transcriptList.map((transcriptItem, i) => <li key={i}>{transcriptItem}</li>)} */}
+            </ul>
+          <p>t {transcript}</p>
+          <p>ft {finalTranscript}</p>
         </div>
-    )
+      )
+    
+    // return (
+    //     <div>Words
+    //        {/* <Lines text={text}/> */}
+    //     </div>
+    // )
 }
+
+// show current transcript as the end
+
