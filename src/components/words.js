d23ca5534b66b67b2528e90f318add6f83e91e7d
gidspen <70455381+gidspen@users.noreply.github.com>
2021-01-13 17:46:38 -0600

    wip

--- src/components/words.js
+++ src/components/words.js
@@ -2,13 +2,13 @@ import React, { useState } from 'react';
 import Lines from './lines';
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 
-
+const transcriptList = [];
 
 export default function Words() {
     const supportedBrowsers = ['Chrome', 'Edge', 'Chrome Android', 'WebView Android', 'Samsumg Internet'];
     const [isActive, setIsActive] = useState(true);
-    const [transcriptList, setTranscriptList] = useState([]);
-    const { transcript, resetTranscript, finalTranscript, listening } = useSpeechRecognition();
+    // const [transcriptList, setTranscriptList] = useState([]);
+    const { transcript, resetTranscript, finalTranscript, listening, interimTranscript } = useSpeechRecognition();
 
     console.log('from the top', listening, isActive);
 
@@ -31,37 +31,49 @@ export default function Words() {
     console.log('is listening: ', listening);
 
     async function start() {
-        console.log('started startListening');
-        await SpeechRecognition.startListening({ continuous: false })
-        console.log('finished startListening');
+        try {
+            console.log('started startListening');
+            await SpeechRecognition.startListening({ continuous: false })
+            console.log('finished startListening');
+
+            if (!listening && isActive) {
+                console.log('inside the big if!');
+                
+                if (transcriptList[transcriptList.length - 1 ] !== finalTranscript ) {
+                    console.log('setting transcript');
+                    // setTranscriptList([...transcriptList, transcript]);
+
+                    transcriptList.push(finalTranscript);
+                    console.log('transcript set');
+                }
+                console.log('the current tList: ', transcriptList);
+        
+        
+            };
+        } catch (error) {
+            console.log(error);
+        }
     }
 
     async function stop() {
-        console.log('stopping');
-        console.log('final t: ', finalTranscript);
-        await SpeechRecognition.stopListening()
-        console.log('final y after: ', finalTranscript);
-        await setIsActive(false);
-
-        console.log('completed stop');
+        try {
+            console.log('stopping');
+            await SpeechRecognition.stopListening()
+            await setIsActive(false);
+            console.log('stopped');
+        } catch (error) {
+            console.log(error);
+        }
     }
+    
 
     console.log('not listening and is active: ', (!listening && isActive));
 
     // appends the transcript once the we stop listening
     // then starts listening again
-    if (!listening && isActive) {
-        console.log('inside the big if!');
-        
-        if (transcriptList[transcriptList.length - 1 ] !== finalTranscript ) {
-            console.log('setting transcript');
-            setTranscriptList([...transcriptList, transcript]);
-            console.log('transcript set');
-        }
-        console.log('the current tList: ', transcriptList);
+    
 
-        start();
-    };
+    start();
 
     return (
         <div>
@@ -69,10 +81,11 @@ export default function Words() {
           <button onClick={stop}>Stop</button>
           <button onClick={resetTranscript}>Reset</button>
             <ul>
-              {/* {transcriptList.map((transcriptItem, i) => <li key={i}>{transcriptItem}</li>)} */}
+              {transcriptList.map((transcriptItem, i) => <li key={i}>{transcriptItem}</li>)}
             </ul>
           <p>t {transcript}</p>
-          <p>ft {finalTranscript}</p>
+          {/* <p>ft {finalTranscript}</p>*/}
+          <p>tlist {transcriptList}</p>
         </div>
       )
     
