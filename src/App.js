90190ec0c921d7191742df4c1c8a4f9405bc49e2
Trevor Davis <trevormckay.davis@gmail.com>
2021-01-14 07:08:06 -0600

    refactored welcome into component

--- src/App.js
+++ src/App.js
@@ -1,44 +1,17 @@
 import './App.css';
 import Words from './components/words';
 import Sorry from './components/sorry';
+import Welcome from './components/welcome';
 import SpeechRecognition from 'react-speech-recognition';
 
 function App() {
+  const supported = SpeechRecognition.browserSupportsSpeechRecognition();
   return (
     <div className='App'>
       <header className='App-header'>My Meditation Notes</header>
       <div className='content'>
-        <div className='recorder'>
-          {SpeechRecognition.browserSupportsSpeechRecognition() ? (
-            <Words />
-          ) : (
-            <Sorry />
-          )}
-        </div>
-        <div className='welcome'>
-          <h3>Welcome to My Meditation App!</h3>
-          <p>
-            Here you can record your thoughts and emotions with out having to
-            break your meditation. No more writting down that epiphiny you dont
-            want to forget, but runing your flow!
-          </p>
-          <p>
-            To get started, just press start! when you are finished you can just
-            press stop or leave the page!
-          </p>
-          <p>
-            If you want ti kate your notes with you, just copy and paste the
-            text into your journalling/note taking app of your choice :)
-          </p>
-          <p>
-            If you have any feedback, feel free to let us know using any of the
-            links below!
-            <a href=''>Twitter</a>
-            <a href='https://github.com/gidspen/my-meditation-notes/issues'>
-              GitHub
-            </a>
-          </p>
-        </div>
+        <div className='recorder'>{supported ? <Words /> : <Sorry />}</div>
+        <Welcome />
       </div>
     </div>
   );
---
+++ src/components/welcome.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+export default function Welcome() {
+  return (
+    <div className='welcome'>
+      <h3>Welcome to My Meditation App!</h3>
+      <p>
+        Here you can record your thoughts and emotions with out having to break
+        your meditation. No more writting down that epiphiny you dont want to
+        forget, but runing your flow!
+      </p>
+      <p>
+        To get started, just press start! when you are finished you can just
+        press stop or leave the page.
+      </p>
+      <p>
+        If you want ti kate your notes with you, just copy and paste the text
+        into your journalling/note taking app of your choice :)
+      </p>
+      <p>
+        If you have any feedback, feel free to let us know using any of the
+        links below!
+      </p>
+      <a href=''>Twitter</a>
+      <a href='https://github.com/gidspen/my-meditation-notes/issues'>GitHub</a>
+    </div>
+  );
+}
