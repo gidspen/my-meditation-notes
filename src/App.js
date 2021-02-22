fe6151a3af340b16bb990fe68d5544f48adf8fbc
gidspen <70455381+gidspen@users.noreply.github.com>
2021-02-22 16:49:29 -0600

    Testing Sentry

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
 
   return (
     <div className="App">
+      <button onClick={()=> breakTheWorld()}>BreakIt!</button>
       <header className="App-header">
         My Meditation Notes
       </header>
