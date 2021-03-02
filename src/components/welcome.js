2b0f8d5ba9224a62a6de23164f20b4b0fd01b4ec
gidspen <70455381+gidspen@users.noreply.github.com>
2021-03-02 16:07:54 -0600

    wip

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -6,24 +6,26 @@ export default function Welcome() {
       {/* <h3>Welcome to My Meditation App!</h3> */}
       <p>
       My Meditation Notes is a voice dictation app designed to capture the 
-      creative thoughts that arise during meditation, allowing you to 
-      seemlessly remain in a meditative state while engaging your muse.
+      distracting or creative thoughts that arise during meditation, allowing you to 
+      seemlessly remain in a meditative state.
       </p>
       <p>
-        To begin, simply press Start. To end, press Stop. When the light to 
-        the right of the Stop button is Red, it's recording. When Green, it's not. 
-        To clear the notepad, you can refresh the page.
+        To begin, simply press Start and begin your meditative practice. If a thought crosses
+        your mind that you want to capture, speak the note you want to keep and it will be recorded
+        on the notepad. To end, press Stop.
       </p>
       <p>
         None of the notes are saved so if you wish to keep them, be sure to 
         copy/paste them into a file you can save.
       </p>
       <p>
-        If you have any feedback or find this useful, shoot us an email at gidspen@gmail.com or tweet us below! 
+        If you found benefit from this, share the love by upvoting it wherever you found it or
+        telling a friend.
+        If you have any feedback or find this useful, shoot us an email at gidspen@gmail.com! 
         We'd love to hear from you.
       </p>
-      <a href='https://twitter.com/ToolsBetter'>Twitter</a>
-      <a href='https://github.com/gidspen/my-meditation-notes/issues'>GitHub</a>
+      {/* <a href='https://twitter.com/ToolsBetter'>Twitter</a>
+      <a href='https://github.com/gidspen/my-meditation-notes/issues'>GitHub</a> */}
     </div>
   );
 }
